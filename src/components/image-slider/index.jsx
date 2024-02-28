048d84a238a06928ceb8b21fe0ec13518483251e
Mohat Shil <120472338+shilCode@users.noreply.github.com>
2024-02-28 20:59:58 +0000

    1:27:07

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
+import './styles.css'
 
 export default function ImageSlide({url,limit}){
     const [images,setImages] = useState([])
@@ -22,6 +24,14 @@ export default function ImageSlide({url,limit}){
         }
     }
 
+    function handlePrev(){
+        setCurrentSlide(currentSlide===0?images.length-1:currentSlide-1)
+    }
+
+    function handleNext(){
+        setCurrentSlide(currentSlide===images.length?0:currentSlide+1)
+    }
+
     useEffect(()=>{
         if(url!=='')fetchImages(url)
     },[url])
@@ -31,5 +41,31 @@ export default function ImageSlide({url,limit}){
     if(loading) return <div>Loading data please wait</div>
     if(errorMsg!==null) return <div>Error occurred{errorMsg}</div>
     return <div className="container">
+        <BsArrowLeftCircleFill onClick={handlePrev} className="arrow arrow-left"/>
+        {
+             images && images.length ?
+             images.map((imagesItem,index)=>(
+                 <img
+                 key={imagesItem.id}
+                 alt={imagesItem.download_url}
+                 src={imagesItem.download_url}
+                 className={currentSlide===index?"current-image":"current-image hide-current-image"}
+                 />
+             ))
+            : null 
+        }
+        <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right"/>
+       <span className="circle-indicator">
+       {
+            images && images.length ?
+            images.map((_,index)=>(
+                <button key={index} className={currentSlide===index?"current-indicator":"current-indicator inactive-indicator"}
+                onClick={()=>setCurrentSlide(index)}
+                
+                ></button>
+            ))
+            : null
+        }
+       </span>
     </div>
 }
